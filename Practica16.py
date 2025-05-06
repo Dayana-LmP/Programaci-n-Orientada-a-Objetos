6d7dd1ce2dc9d64dc978f1f7bd5ddfc684228ec1
Dayana-LmP <lamaspinedodayana@gmail.com>
2025-05-06 08:51:28 -0600

    UPDATE

---
+++ Practica16.py
@@ -0,0 +1,23 @@
+#Práctica 16: Hilos y red
+import threading 
+import time
+
+class hilo(threading.thread):
+    def __init__(self, nombre, intervalo):
+        super().__init__()
+        self.nombre = nombre
+        self.intervalo = intervalo
+
+    def run(self):
+        print(f"El hilo {self.nombre} ha comenzado")
+        for i in range(5):
+            print(f" El hilo {self.nombre} se encuentra en iteración{i}")
+            time.sleeep(self.intervalo)
+        print(f"El hilo {self.nombre} ha finalizado")
+hilo1 = hilo("hilo1", 2)
+hilo2 = hilo("hilo2", 4)
+
+hilo1.start()
+hilo2.start()
+hilo1.join() #Para que no quede en memoria
+hilo2.join()
