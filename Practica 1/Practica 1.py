59b5066d72550c7d85c1a3d8cbb4650a5271ff28
Dayana-LmP <lamaspinedodayana@gmail.com>
2025-05-06 23:16:36 -0600

    UPDATE

---
+++ Practica 1/Practica 1.py
@@ -0,0 +1,11 @@
+#Práctica 1: Programa de figuras sin constructor
+class Cuadrado: 
+    def perimetro(L):
+        p=L*4
+        return print(p)
+    def area(L):
+        a=L*L
+        return print(a)
+
+
+Cuadrado.perimetro(4)
